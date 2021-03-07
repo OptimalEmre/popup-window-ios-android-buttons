d888873c0045ec023e6679e8bcdc0fe550b1ca7c
OptimalEmre <emre@optimalsystems.dev>
2021-03-07 18:50:17 +0300

    added transparent background for the popup

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let userAgent = detect.parse(navigator.userAgent);
 const popupWindow = document.getElementById('download-popup');
+const popupWindowBackground = document.getElementById('popup-background');
 const popupCloseButton = document.getElementById('popup-close-button');
 
 if (userAgent) {
@@ -16,13 +17,16 @@ function triggerPopup(divIdPrefix) {
     if(button) {
         button.style.display = "block";
         setTimeout(() => {
+            popupWindowBackground.classList.add('popup-window--active');
             popupWindow.classList.add('popup-window--active');
+            popupCloseButton.focus();
         }, 2000);
     }
 }
 
 if (popupWindow) {
         popupCloseButton.addEventListener('click', () => {
+            popupWindowBackground.classList.add('popup-window--active');
             popupWindow.classList.remove('popup-window--active');
         });
 }
